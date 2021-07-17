b8b8cc4b8ae3272d0abdf1d18f2d417cafaa28ac
Ekaterina Pushkova <pushkova1666@gmail.com>
2021-07-17 19:52:56 +0300

    correct position of the picture on the screen

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 //===============================
 let image = document.getElementById('image');
 let imgs = new Array(
-    '/images/w1.jpg', 
-    '/images/w2.jpg', 
-    '/images/w3.jpg', 
-    '/images/w4.jpg', 
-    '/images/w5.jpg', 
-    '/images/w6.jpg', 
-    '/images/w7.jpg', 
-    '/images/w8.jpg', 
-    '/images/w9.jpg', 
-    '/images/w10.jpg');
+    'images/w1.jpg', 
+    'images/w2.jpg', 
+    'images/w3.jpg', 
+    'images/w4.jpg', 
+    'images/w5.jpg', 
+    'images/w6.jpg', 
+    'images/w7.jpg', 
+    'images/w8.jpg', 
+    'images/w9.jpg', 
+    'images/w10.jpg');
 image.src = imgs[0];
 let i = 1;
 function changeImgs(){
@@ -23,3 +23,5 @@ function changeImgs(){
         i = 0;
     }
 }
+
+
